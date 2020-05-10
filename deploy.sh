2c58f328efd621200672c50779ba72649ee69bac
Arnoud Kleinloog <arnoud.kleinloog@my-health.cloud>
2020-05-10 22:28:29 +0200

    deploy script and static content

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+echo "Cleaning Publish Folder"
+
+rm -r ./public/*
+
+echo "Running Hugo Build"
+
+env HUGO_ENV="production" hugo --gc --minify -t academic -b https://my-health-cloud.github.io
+
+cd public
+
+echo "Adding changes to git"
+
+git add .
+
+msg="regenerating site content $(date)"
+
+git commit -m "$msg"
+
+echo "Pushing to master"
+
+git push origin master
+
+cd ..
+
+echo "Deployment Finished"
---
+++ static/README.md
@@ -0,0 +1,7 @@
+# Generated Website Content
+
+The repository contains the generated website content for the My Health website.
+
+It will be overwritten whenever the content is regenerated from the sources contained in [source repository](https://github.com/my-health-cloud/website).
+
+
